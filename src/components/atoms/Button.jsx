fdfa5dc04ba0c4134d326eb1d97bf4ae8145ebac
fapeCTM <fa.morande@duocuc.cl>
2025-11-06 23:18:33 -0300

    se me olvido un atomo XD

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button as BootstrapButton } from 'react-bootstrap';
-import '../../styles/atoms/button.css';
 
 function Button({ children, ...props }) {
     return <BootstrapButton {...props}>{children}</BootstrapButton>;
