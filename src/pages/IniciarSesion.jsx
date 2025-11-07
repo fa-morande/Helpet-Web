a040a39acd05c4ac453b8a04b478fd6d81e6266b
fapeCTM <fa.morande@duocuc.cl>
2025-11-07 12:17:25 -0300

    ya esta la base, solo falta kizazo nomas

---
+++ src/pages/IniciarSesion.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function IniciarSesion() {
+    return (
+        <div>
+            <h1>Aqui falta manteca</h1>
+            <p>pagina de iniciar sesion</p>
+        </div>
+    );
+}
+
+export default IniciarSesion;
